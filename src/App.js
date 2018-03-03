bca4563866fa387bb096f2427e106f56a17c33a2
Ars Bhatti <arsh.bhatti8@gmail.com>
2018-03-02 16:57:46 -0800

    Commit before implementing CSS Modules: Added gitIgnore for workspace.xml

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomepageLayout from './components/homepagelayout/homepageLayout';
-import {ResponsiveContainer} from 'semantic-ui-react';
+
 
 class App extends Component {
 
--- src/components/mobilecontainer/mobileContainer.js
+++ src/components/mobilecontainer/mobileContainer.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import {Responsive, Sidebar,Menu,Segment,Container,Icon,Button} from 'semantic-ui-react';
+import {Responsive, Sidebar,Menu,Segment,Container,Icon} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import HomepageHeading from '../homepageheading/hompageHeading';
 
